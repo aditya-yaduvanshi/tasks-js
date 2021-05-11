4750ee1ed3c7f9b5bfcab5deca3cea29af71a4bd
adityayaduvanshi0001 <adityayaduvanshi0001@gmail.com>
2021-05-11 18:45:57 +0530

    js updated

--- script.js
+++ script.js
@@ -3,6 +3,28 @@ document.addEventListener('DOMContentLoaded',()=>{
   const submit = document.querySelector('#submit');
 
   submit.disabled = true;
+  newtask.onkeyup = ()=>{
+    if (newtask.value.length>0){
+      submit.disabled = false;
+    }
+    else {
+      submit.disabled = true;
+    }
+  }
 
+  document.querySelector('form').onsubmit = ()=>{
+    const task = newtask.value;
+    const li = document.createElement('li');
+    li.innerHTML = task;
+
+    document.querySelector('#tasks').append(li);
+
+    newtask.value='';
+    submit.disabled = true;
+
+    return false;
+  }
   
+
+
 });
