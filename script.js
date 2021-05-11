1fa5626828ad845daa24fc569a1196e039f3b577
adityayaduvanshi0001 <adityayaduvanshi0001@gmail.com>
2021-05-11 17:52:22 +0530

    update js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const task = document.getElementById("task");
-
+document.querySelector("#task-list").innerHTML = "No Task";
 function newtask(){
   if (task){
     document.getElementById("task-list").innerHTML += document.createElement('li', task);
